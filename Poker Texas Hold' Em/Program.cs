29cc0e4326fd9fc4d7918aaa987c859eb11e6d5b
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-09-19 12:36:14 +0200

    Bienvenida

---
+++ Poker Texas Hold' Em/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Texas_Hold__Em
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenidos al Póker Texas Hold' Em!");
+        }
+    }
+}
